c4561d26be34cbc4a2a719a32165d38c1fb27d36
sharipovvzoirbek <zabyjorda@gmail.com>
2023-09-19 14:29:28 +0500

    misol yechildi

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -1,11 +1,12 @@
 #Create a variable called 'number' and assign it the three-digit number.
-
+number = 245
 #Find the 'number' first digit and assign to x1.
-
+x1 = 2
 #Find the 'number' second digit and assign to x2.
-
+x2 = 4
 #Find the 'number' third digit and assign to x3.
-
+x3 = 5
 #Create a variable called 'answer' and assign it the sum of the three digits x1, x2, x3.
-
+answer = x1+x2+x3
 #Print the value of the 'answer.
+print(answer)
